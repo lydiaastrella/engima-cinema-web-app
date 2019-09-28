e80bb0db302d1cfc7fbc7eac8a9a7aa5957bbf75
Akhmal <13517127@std.stei.itb.ac.id>
2019-09-28 19:21:12 +0700

    add page structure

---
+++ page/book_ticket.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Transaction History</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+	<link rel="stylesheet" href="../css/style.css">
+	<link rel="stylesheet" href="../css/book_ticket.css">
+</head>
+<body>
+	<div class="navbar"> 
+		<div class="navbar-box">
+			<a id="logo"><strong>Engi</strong>ma</a>
+			<input id="search-bar" type="text" placeholder="Search Movie">
+			<a class="menu">Logout</a>
+			<a class="menu">Transaction</a>
+		</div>
+	</div>
+	<div class="container">
+		<div class="content">
+			<div class="grid-container">
+				<div class="header">
+					P
+				</div>
+				<div class="left">P</div>
+				<div class="right">P</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
