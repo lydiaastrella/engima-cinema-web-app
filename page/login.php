3ca4ff6056ac6c3cace6417b4435c7d37f1add63
Eka Sunandika <13517130@std.stei.itb.ac.id>
2019-09-28 18:21:55 +0700

    fix submit input

---
+++ page/login.php
@@ -0,0 +1,30 @@
+﻿<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title></title>
+    <link rel = "stylesheet" href="style1.css">
+</head>
+
+<body>
+<form action = "login_handler.php" method = "post">
+  <div class= "head1">
+    <p>Welcome to <strong>Engi</strong>ma!</p>
+  </div>
+  <div class="container">
+    <label for="email">Email</label>
+    <input type="text" placeholder="john@doe.com" name="email" required>
+
+    <label for="psw">Password</label>
+    <input type="password" placeholder="place here" name="psw" required>
+        
+	<input type="submit" name = "create" value = "Login">
+    <div>
+	  <p style="text-align:center;"><b>Don't have an account? <a href="//www.google.com">Register here</a></b></p>
+	</div>
+  </div>
+</form>
+
+</body>
+</html>
