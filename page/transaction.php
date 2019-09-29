c79dcb233b754c4267ba4965380a2eba2742d985
Akhmal <13517127@std.stei.itb.ac.id>
2019-09-29 11:50:54 +0700

    add css formatting

--- page/transaction.php
+++ page/transaction.php
@@ -4,6 +4,7 @@
 	<title>Transaction History</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
 	<link rel="stylesheet" href="../css/style.css">
+	<link rel="stylesheet" href="../css/transaction.css">
 </head>
 <body>
 	<div class="navbar"> 
--- php/transaction.php
+++ php/transaction.php
@@ -1,4 +1,22 @@
 <?php
+	function stringMonth($number_month) {
+		switch ($number_month) {
+			case 1: return "January"; break;
+			case 2: return "February"; break;
+			case 3: return "March"; break;
+			case 4: return "April"; break;
+			case 5: return "May"; break;
+			case 6: return "June"; break;
+			case 7: return "July"; break;
+			case 8: return "August"; break;
+			case 9: return "September"; break;
+			case 10: return "October"; break;
+			case 11: return "November"; break;
+			case 12: return "December"; break;
+			default: return "Error"; break;
+		}
+	}
+
 	$servername = 'localhost';
 	$username = 'root';
 	$password = '';
@@ -18,24 +36,15 @@
 
 	if ($general->num_rows > 0) {
 	    while ($row = $general->fetch_assoc()) {
-	        echo "<div class='film-box'><img src='".$row['film_photo']."'>".$row['title']."<span><p class='schedule_text'>Schedule: </a><a>".$row['date']." ".$row['time']."</a></span></div>";
+	    	$tanggal = $row['date'];
+	    	$date_result = explode("-",$tanggal);
+	    	$tahun = $date_result[0];
+	    	$bulan = $date_result[1];
+	    	$hari = $date_result[2];
+	    	$bulan_huruf = stringMonth($bulan);
+	        echo "<div class='film-box'><img src='".$row['film_photo']."'><h3>".$row['title']."</h3><span><a class='schedule_text'>Schedule: </a><a>".$bulan_huruf." "."$hari".", ".$tahun." - ".$row['time']."</a></span></div>";
 	    }
 	} else {
     	echo "0 results";
 	}
-
-	/*$tanggal = $_POST['tanggal'];
-	$waktu = $_POST['waktu'];
-	$url_foto = $_POST['url_foto'];*/
-
-	//$data = mysqli_query($conn, "select email, password from user where email='$email' and password='$password'");
-
-	//$cek = mysqli_num_rows($data);
-
-	/*if($cek > 0){
-		header("location:home.php");
-	}else{
-		header("location:login.php");
-	}*/
-		echo "<p>Hello world!<p>";
 ?>
