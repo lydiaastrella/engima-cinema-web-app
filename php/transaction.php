b7a22e274098bb8acae2e45bd54c12ecdb759b5b
Akhmal <13517127@std.stei.itb.ac.id>
2019-09-29 11:13:25 +0700

    display query result

--- php/transaction.php
+++ php/transaction.php
@@ -12,10 +12,21 @@
 	    die("Connection failed: " . $conn->connect_error);
 	}
 
-	$judul = $_POST['judul'];
-	$tanggal = $_POST['tanggal'];
+	$general = $conn->query(
+	"select * from film, film_schedule, transaction, schedule_time where 
+	film.id_film = film_schedule.id_film AND film_schedule.id_schedule = transaction.id_schedule AND film_schedule.id_schedule = schedule_time.id_schedule");
+
+	if ($general->num_rows > 0) {
+	    while ($row = $general->fetch_assoc()) {
+	        echo "<div class='film-box'><img src='".$row['film_photo']."'>".$row['title']."<span><p class='schedule_text'>Schedule: </a><a>".$row['date']." ".$row['time']."</a></span></div>";
+	    }
+	} else {
+    	echo "0 results";
+	}
+
+	/*$tanggal = $_POST['tanggal'];
 	$waktu = $_POST['waktu'];
-	$url_foto = $_POST['url_foto'];
+	$url_foto = $_POST['url_foto'];*/
 
 	//$data = mysqli_query($conn, "select email, password from user where email='$email' and password='$password'");
 
